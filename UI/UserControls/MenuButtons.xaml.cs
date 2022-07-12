9993967924eb3523516e67633bd23b9f8f650ba4
AmirHossein Maleki <amir.maleki1371@gmail.com>
2022-07-12 23:12:24 +0430

    UI Design Part 02

--- UI/UserControls/MenuButtons.xaml.cs
+++ UI/UserControls/MenuButtons.xaml.cs
@@ -24,5 +24,29 @@ namespace UI.UserControls
         {
             InitializeComponent();
         }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(MenuButtons));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(MenuButtons));
+
+        public MahApps.Metro.IconPacks.PackIconMaterialKind Icon
+        {
+            get { return (MahApps.Metro.IconPacks.PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(MahApps.Metro.IconPacks.PackIconMaterialKind), typeof(MenuButtons));
     }
 }
