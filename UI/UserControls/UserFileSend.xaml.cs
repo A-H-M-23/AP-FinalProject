4c81dd276d771409d6c12136cecf353bf50118e1
AmirHossein Maleki <amir.maleki1371@gmail.com>
2022-07-15 21:29:12 +0430

    File Send User Control added

---
+++ UI/UserControls/UserFileSend.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.UserControls
+{
+    public partial class UserFileSend : UserControl
+    {
+        public UserFileSend()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty UsernameProperty = DependencyProperty.Register("Username", typeof(string), typeof(UserChat));
+
+        public string Username
+        {
+            get { return (string)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+
+        private void AttachFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
